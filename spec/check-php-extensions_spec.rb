44a890189c8b01ec7c0ff88d7580af97d2e8c511
psy <psych0pat@ymail.com>
2017-09-23 01:56:03 +0200

    move tests

--- spec/check-php-extensions_spec.rb
+++ spec/check-php-extensions_spec.rb
@@ -1,13 +1,12 @@
-require_relative 'spec_helper.rb'
+require_relative './spec_helper.rb'
 require_relative './plugin_stub.rb'
-
 require_relative '../bin/check-php-extensions.rb'
 
 describe CheckPHPExtensions do
   it 'Test valid extension' do
     check = CheckPHPExtensions.new
     check[:extension] = 'date'
-    expect(-> { io.run }).to raise_error SystemExit
+    expect(-> { check.run }).to raise_error SystemExit
   end
 end
 
